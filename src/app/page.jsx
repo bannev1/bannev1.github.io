2b9e1e90df287554dbb407f34e8b89b9e8118874
Valentina Banner <78287030+bannev1@users.noreply.github.com>
2026-06-29 14:04:35 +0800

    add base stuff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,31 @@
+import NavBar from "@/components/NavBar";
+
+
 export default function Home() {
 	return (
 		<>
-			
+			{/* Title bar */}
+			<div id='title'>
+				{/* Title bar - first part */}
+				<div id="first">
+					<h1 className='name'>VALENTINA</h1>
+					
+					<div className='frontPart'/>
+					<div className='backPart'/>
+				</div>
+
+
+				{/* Title bar - last part */}
+				<div id="last">
+					<h1 className='name'>BANNER</h1>
+					
+					<div className='frontPart'/>
+					<div className='backPart'/>
+				</div>
+			</div>
+
+			{/* Nav Bar */}
+			<NavBar homePage={true}/>
 		</>
 	);
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar({
+	homePage = false
+}) {
+	return (
+		<div>NavBar</div>
+	)
+}
+
+export default NavBar
